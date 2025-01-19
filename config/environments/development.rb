6dd09a67fb53545affda1ca34196d03f1ad02142
Viktoriia <marakya750@gmail.com>
2025-01-19 23:41:54 +0100

    Allow AWS LoadBalancer host

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -17,6 +17,9 @@ Rails.application.configure do
   # Enable server timing
   config.server_timing = true
 
+  config.hosts << "a29998ab3b1c3485bb4607d0ab21193b-267473912.eu-central-1.elb.amazonaws.com"
+
+
   # Enable/disable caching. By default caching is disabled.
   # Run rails dev:cache to toggle caching.
   if Rails.root.join("tmp/caching-dev.txt").exist?
